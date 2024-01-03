183dca221d9b71f6ba71c72c20538c447ba238c5
Galasso <galassoworkstation@gmail.com>
2024-01-03 00:22:17 -0300

    Create a SpaceTest class

---
+++ DevDojo/Exercícios/Jogo de Cartas/jogodecartas/src/academy/devdojo/exercicios/jogocartas/test/test.java
@@ -0,0 +1,9 @@
+package academy.devdojo.exercicios.jogocartas.test;
+
+import academy.devdojo.exercicios.jogocartas.domain.Deck;
+
+public class test {
+    public static void main(String[] args) {
+        Deck deck1 = new Deck();
+    }
+}
