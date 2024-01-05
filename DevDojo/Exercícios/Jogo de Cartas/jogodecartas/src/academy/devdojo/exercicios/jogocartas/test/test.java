2882893f73eabbfd27cd8b916952cc4e5ebbcd10
Galasso <galassoworkstation@gmail.com>
2024-01-05 20:42:57 -0300

    Improve how the GameCreationFlows goes

--- DevDojo/Exercícios/Jogo de Cartas/jogodecartas/src/academy/devdojo/exercicios/jogocartas/test/test.java
+++ DevDojo/Exercícios/Jogo de Cartas/jogodecartas/src/academy/devdojo/exercicios/jogocartas/test/test.java
@@ -10,17 +10,19 @@ import java.util.Arrays;
 public class test {
     public static void main(String[] args) {
         Deck deck1 = new Deck();
-        Player player1 = new Player("P1", Games.MORE_THAN);
-        Player player2 = new Player("P2", Games.MORE_THAN);
-        Player player3 = new Player("P3", Games.MORE_THAN);
-        Player [] players = {player1,player2,player3};
-        Game game = new Game(players,deck1.getShuffledDeck(), Games.MORE_THAN);
+        Deck deck2 = new Deck();
+        Game game1 = new Game(deck1.getShuffledDeck(), Games.MORE_THAN);
+        Game game2 = new Game(deck2.getShuffledDeck(), Games.BLACKJACK);
 
-        System.out.println(player1);
-        System.out.println(player2);
-        System.out.println(player3);
-        System.out.println(Arrays.toString(players));
+//        Player player1 = new Player("P1", Games.BLACKJACK);
+//        Player player2 = new Player("P2", Games.BLACKJACK);
+//        Player player3 = new Player("P3", Games.BLACKJACK);
+//        Player[] players = {player1, player2, player3};
+//        Game game = new Game(players, deck1.getShuffledDeck(), Games.BLACKJACK);
 
-        //criar teste para verificar se os jogadores estão recebendo as cartas iniciais
+//        System.out.println(player1);
+//        System.out.println(player2);
+//        System.out.println(player3);
+//        System.out.println(Arrays.toString(players));
     }
 }
