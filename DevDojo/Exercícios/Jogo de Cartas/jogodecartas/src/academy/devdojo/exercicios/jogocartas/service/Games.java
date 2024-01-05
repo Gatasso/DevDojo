4bed93559e3c17b2d6f9f057266884804630ced3
Galasso <galassoworkstation@gmail.com>
2024-01-05 20:43:13 -0300

    Change the BLACKJACK rules

--- DevDojo/Exercícios/Jogo de Cartas/jogodecartas/src/academy/devdojo/exercicios/jogocartas/service/Games.java
+++ DevDojo/Exercícios/Jogo de Cartas/jogodecartas/src/academy/devdojo/exercicios/jogocartas/service/Games.java
@@ -1,10 +1,10 @@
 package academy.devdojo.exercicios.jogocartas.service;
 
 public enum Games {
-    MORE_THAN(2,2),
-    BLACKJACK(3,2);
+    MORE_THAN(2, 2),
+    BLACKJACK(3, 3);
 
-    public final int NUMBER_OF_PLAYERS,INITIAL_CARDS;
+    public final int NUMBER_OF_PLAYERS, INITIAL_CARDS;
 
     Games(int NUMBER_OF_PLAYERS, int INITIAL_CARDS) {
         this.NUMBER_OF_PLAYERS = NUMBER_OF_PLAYERS;
