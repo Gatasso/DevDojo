e2e4db4ac156aeca63abf739e6350c6ba8c45f34
Galasso <galassoworkstation@gmail.com>
2024-01-09 20:34:05 -0300

    New test area, creating the game by the chosen mod, creating the Deck,Players after the game initialize

---
+++ DevDojo/Exercícios/Jogo de Cartas/jogodecartas/src/academy/devdojo/exercicios/jogocartas/test/test2.java
@@ -0,0 +1,10 @@
+package academy.devdojo.exercicios.jogocartas.test;
+
+import academy.devdojo.exercicios.jogocartas.service.Game;
+import academy.devdojo.exercicios.jogocartas.service.Games;
+
+public class test2 {
+    public static void main(String[] args) {
+        Game game = new Game(Games.MORE_THAN);
+    }
+}
