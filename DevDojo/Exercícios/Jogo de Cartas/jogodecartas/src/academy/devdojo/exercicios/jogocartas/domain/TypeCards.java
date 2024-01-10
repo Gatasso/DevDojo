23aba6ead62c25fbf5750f70d5edcc3a9f728ebb
Galasso <galassoworkstation@gmail.com>
2024-01-09 23:30:09 -0300

    Assign Values to each type of card

--- DevDojo/Exercícios/Jogo de Cartas/jogodecartas/src/academy/devdojo/exercicios/jogocartas/domain/TypeCards.java
+++ DevDojo/Exercícios/Jogo de Cartas/jogodecartas/src/academy/devdojo/exercicios/jogocartas/domain/TypeCards.java
@@ -1,6 +1,20 @@
 package academy.devdojo.exercicios.jogocartas.domain;
 
 public enum TypeCards {
-    ACE, KING, QUEEN, JACK,
-    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN
+    ACE(1), KING(10), QUEEN(10), JACK(10),
+    TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10);
+
+    private int cardValue;
+    /****************************************************************
+     constructors */
+    TypeCards(int cardValue) {
+        this.cardValue = cardValue;
+    }
+
+    /****************************************************************
+     getters and setters*/
+    public int getCardValue() {
+        return cardValue;
+    }
 }
+
