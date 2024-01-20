308f35b5109d5856d6ca7102546e92eb2e4fd816
Galasso <galassoworkstation@gmail.com>
2024-01-20 18:44:34 -0300

    Now the MORE_THAN game mod has a win condition

--- DevDojo/Exercícios/Jogo de Cartas/jogodecartas/src/academy/devdojo/exercicios/jogocartas/service/Games.java
+++ DevDojo/Exercícios/Jogo de Cartas/jogodecartas/src/academy/devdojo/exercicios/jogocartas/service/Games.java
@@ -2,18 +2,27 @@ package academy.devdojo.exercicios.jogocartas.service;
 
 import academy.devdojo.exercicios.jogocartas.domain.Player;
 
-public enum Games {
+public enum Games implements GameRules{
     MORE_THAN(2, 2) {
         @Override
-        public boolean winCondition() {
-            return false;
+        public Player winCondition(Player[] players) {
+            int index = 0;
+            int[] positionPlayer = new int[NUMBER_OF_PLAYERS];
+                for (Player player : players) {
+                    positionPlayer[index] =  player.getTotalValueCards();
+                    index++;
+                }
+            if (positionPlayer[0] > positionPlayer[1]){
+                return players[0];
+            } else if (positionPlayer[0] < positionPlayer[1]) {
+                return players[1];
+            }else return null;
         }
     },
     BLACKJACK(3, 2) {
-
         @Override
-        public boolean winCondition() {
-            return false;
+        public Player winCondition(Player[] players) {
+            return players[0];
         }
     };
 
@@ -25,6 +34,5 @@ public enum Games {
         this.NUMBER_OF_PLAYERS = NUMBER_OF_PLAYERS;
         this.INITIAL_CARDS = INITIAL_CARDS;
     }
-
-    public abstract boolean winCondition();
 }
+
