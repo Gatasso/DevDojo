dfbe6bc216b851236f0745e50bb9491508911c28
Galasso <galassoworkstation@gmail.com>
2024-01-20 18:44:49 -0300

    Environment to run the game

---
+++ DevDojo/Exercícios/Jogo de Cartas/jogodecartas/src/academy/devdojo/exercicios/jogocartas/test/RunningGame.java
@@ -0,0 +1,10 @@
+package academy.devdojo.exercicios.jogocartas.test;
+
+import academy.devdojo.exercicios.jogocartas.service.Game;
+import academy.devdojo.exercicios.jogocartas.service.Games;
+
+public class RunningGame {
+    public static void main(String[] args) {
+        new Game(Games.MORE_THAN);
+    }
+}
