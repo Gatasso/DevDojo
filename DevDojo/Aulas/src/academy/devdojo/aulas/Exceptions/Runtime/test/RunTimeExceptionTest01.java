8cce3077f1d7dcc158c761a0665098097479fce1
Galasso <galassoworkstation@gmail.com>
2024-01-31 14:44:40 -0300

    RunTimeException Class

---
+++ DevDojo/Aulas/src/academy/devdojo/aulas/Exceptions/Runtime/test/RunTimeExceptionTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.aulas.Exceptions.Runtime.test;
+
+public class RunTimeExceptionTest01 {
+    public static void main(String[] args) {
+        Object object = null;
+        int[] nums = {1,2};
+        System.out.println(object.toString());
+        System.out.println(nums[2]);
+    }
+}
