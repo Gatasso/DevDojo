1abb0d34aa38da16acbd526a853c8ab6ead4dfec
Galasso <galassoworkstation@gmail.com>
2024-02-02 14:00:23 -0300

    Question 6 answer

--- DevDojo/Aulas/src/academy/devdojo/aulas/Exceptions/exercicios/Resolucao.java
+++ DevDojo/Aulas/src/academy/devdojo/aulas/Exceptions/exercicios/Resolucao.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 public class Resolucao {
     public static void main(String[] args) {
 //        System.out.println(division(1,0));
+//*********************************************************
 //        createFile();
+//*********************************************************
 //     getIdade(101);
 //        try {
 //            createFile();
@@ -14,9 +16,10 @@ public class Resolucao {
 //            e.printStackTrace();
 //            System.out.println("Arquivo não criado");
 //        }
-        int[] pair = {2, 4, 6, 8, 0};
-        int[] odd = {1, 3, 5, 7, 9};
-        pairOrOdd(pair, odd);
+//*********************************************************
+//        int[] pair = {2, 4, 6, 8, 0};
+//        int[] odd = {1, 3, 5, 7, 9};
+//        pairOrOdd(pair, odd);
     }
 
     //    public static int division(int a, int b){
@@ -27,7 +30,7 @@ public class Resolucao {
 //            e.printStackTrace();
 //        } return 0;
 //    }
-//
+//*********************************************************
 //    public static void createFile(){
 //        File file = new File("Arquivo/newFile.txt");
 //        try {
@@ -38,36 +41,39 @@ public class Resolucao {
 //            System.out.println("Arquivo não criado");
 //        }
 //    }
+//*********************************************************
 //    public static void getIdade(int age){
 //        if (age < 0 || age >= 100){
 //            throw new IllegalArgumentException("Idade inválida");
 //        }
 //        System.out.println("Idade válida");
 //    }
+//*********************************************************
 //        public static void createFile()throws IOException{
 //        File file = new File("Arquivo/newFile2.txt");
 //        file.createNewFile();
 //        System.out.println("Arquivo criado com sucesso em: " + file.getPath());
 //    }
-    public static void pairOrOdd(int[] pair, int[] odd) {
-        int[] pairAndOdd = new int[5];
-        try {
-                for (int i = 0; i < odd.length; i++) {
-                    pairAndOdd[i] =  odd[i] / pair[i];
-                }
-
-            for (int i = 0; i < pairAndOdd.length; i++) {
-                System.out.println(pairAndOdd[i]);
-            }
-        } catch (ArithmeticException e) {
-            e.printStackTrace();
-            System.out.println("Division could not be done. Math error.");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            e.printStackTrace();
-            System.out.println("Out of bounds. More arguments than expected.");
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-            System.out.println("Invalid argument. Only numbers acceptable");
-        }
-    }
+//*********************************************************
+//    public static void pairOrOdd(int[] pair, int[] odd) {
+//        int[] pairAndOdd = new int[5];
+//        try {
+//                for (int i = 0; i < odd.length; i++) {
+//                    pairAndOdd[i] =  odd[i] / pair[i];
+//                }
+//
+//            for (int i = 0; i < pairAndOdd.length; i++) {
+//                System.out.println(pairAndOdd[i]);
+//            }
+//        } catch (ArithmeticException e) {
+//            e.printStackTrace();
+//            System.out.println("Division could not be done. Math error.");
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            e.printStackTrace();
+//            System.out.println("Out of bounds. More arguments than expected.");
+//        } catch (IllegalArgumentException e) {
+//            e.printStackTrace();
+//            System.out.println("Invalid argument. Only numbers acceptable");
+//        }
+//    }
 }
