195ab2b192a528c2b6bc5c3fb84b3e5bfeff90e9
Galasso <galassoworkstation@gmail.com>
2024-02-02 20:01:47 -0300

    Aula 06

---
+++ DevDojo/Aulas/src/academy/devdojo/aulas/Exceptions/Runtime/test/RunTimeExceptionTest04.java
@@ -0,0 +1,21 @@
+package academy.devdojo.aulas.Exceptions.Runtime.test;
+
+import javax.sound.midi.Soundbank;
+
+public class RunTimeExceptionTest04 {
+    public static void main(String[] args) {
+        try {
+            throw new RuntimeException();
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Dentro do IndexOutOfBoundsException");
+        }catch (IllegalArgumentException e){
+            System.out.println("Dentro do IllegalArgumentException");
+        }catch (ArithmeticException e){
+            System.out.println("Dentro do ArithmeticException");
+        }catch (RuntimeException e){
+            System.out.println("Dentro do RuntimeException");
+        }
+    }
+}
