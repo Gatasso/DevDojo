f708fa538507f414cc90972e19f4f4831bf21c03
Galasso <galassoworkstation@gmail.com>
2024-02-02 13:15:10 -0300

    Exercises Answers

---
+++ DevDojo/Aulas/src/academy/devdojo/aulas/Exceptions/exercicios/Resolucao.java
@@ -0,0 +1,57 @@
+package academy.devdojo.aulas.Exceptions.exercicios;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Resolucao {
+    public static void main(String[] args) {
+//        System.out.println(division(1,0));
+//        createFile();
+//     getIdade(101);
+//        try {
+//            createFile();
+//        }catch (IOException e){
+//            e.printStackTrace();
+//            System.out.println("Arquivo não criado");
+//        }
+    }
+//    public static int division(int a, int b){
+//        try {
+//            return a/b;
+//        } catch (ArithmeticException e){
+//            System.out.println("Não pode se dividir por 0");
+//            e.printStackTrace();
+//        } return 0;
+//    }
+//
+//    public static void createFile(){
+//        File file = new File("Arquivo/newFile.txt");
+//        try {
+//            file.createNewFile();
+//            System.out.println("Arquivo criado com sucesso: " + file);
+//        }catch (IOException e){
+//            e.printStackTrace();
+//            System.out.println("Arquivo não criado");
+//        }
+//    }
+//    public static void getIdade(int age){
+//        if (age < 0 || age >= 100){
+//            throw new IllegalArgumentException("Idade inválida");
+//        }
+//        System.out.println("Idade válida");
+//    }
+//        public static void createFile()throws IOException{
+//        File file = new File("Arquivo/newFile2.txt");
+//        file.createNewFile();
+//        System.out.println("Arquivo criado com sucesso em: " + file.getPath());
+//    }
+    public static void checkEverything(){
+        try {
+
+        } catch (IllegalArgumentException){
+
+        }catch (){
+
+        }
+    }
+}
